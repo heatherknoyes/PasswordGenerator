ae4dfa27a79de0194d08b04c74e605ebf84706e1
Heather Noyes <heatherknoyes@gmail.com>
2022-03-17 15:28:44 -0400

    Added initial password length check and initial password library for characters

--- script.js
+++ script.js
@@ -6,9 +6,33 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
+
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  var characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    /* need to add quotes */
+    specialCharacters: " !#$%&()*+,-./:;<=>?@[\]^_`{|}~",
+    numbers: "0123456789"
+  }
+
+  var passwordLength = window.prompt("Please enter the length of your password: ");
+  if (passwordLength < 8) {
+    window.alert("Password must be equal to or longer than 8 characters. Please try again.");
+  } else if (passwordLength > 128) {
+    window.alert("Password must be equal to or smaller than 128 characters. Please try again.");
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
