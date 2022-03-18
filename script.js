ac6936df68218cd0e6d66da67eddd8f85738760e
Heather Noyes <heatherknoyes@gmail.com>
2022-03-17 21:50:54 -0400

    Create the prompt messages for different types of characters.

--- script.js
+++ script.js
@@ -21,12 +21,34 @@ function generatePassword() {
     numbers: "0123456789"
   }
 
+  var passwordCriteria = "";
+
   var passwordLength = window.prompt("Please enter the length of your password: ");
   if (passwordLength < 8) {
     window.alert("Password must be equal to or longer than 8 characters. Please try again.");
   } else if (passwordLength > 128) {
     window.alert("Password must be equal to or smaller than 128 characters. Please try again.");
   }
+
+  var numberConfirm = window.confirm("Do you want lowercase letters in your password?");
+  var lowerConfirm = window.confirm("Do you want uppercase letters in your password?");
+  var upperConfirm = window.confirm("Do you want numbers in your password?");
+  var specialConfirm = window.confirm("Do you want special characters in your password?");
+
+  if (numberConfirm) {
+    passwordCriteria += characters.numbers;
+  }
+  if (lowerConfirm) {
+    passwordCriteria += characters.lowercase;
+  }
+  if (upperConfirm) {
+    passwordCriteria += characters.uppercase;
+  }
+  if (specialConfirm) {
+    passwordCriteria += characters.specialCharacters;
+  }
+
+  console.log(passwordCriteria);
 }
 
 // Add event listener to generate button
